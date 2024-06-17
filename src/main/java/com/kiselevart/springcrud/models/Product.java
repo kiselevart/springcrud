1a5f8ab1cfad47fff4434413399ac2110ad21308
kiselevart <kiselevart1403@gmail.com>
2024-06-17 17:38:11 +0700

    first commit

---
+++ src/main/java/com/kiselevart/springcrud/models/Product.java
@@ -0,0 +1,33 @@
+package com.kiselevart.springcrud.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "product")
+@EqualsAndHashCode(callSuper = false)
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String name;
+
+    private long quantity;
+    private double unitPrice;
+}
